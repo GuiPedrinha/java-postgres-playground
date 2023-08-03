3844f82d8deb7395faa8b59a83599c61e7189b41
Guilherme Carlos <guilherme.fpedrinha@gmail.com>
2023-08-03 17:32:46 +0000

    Após aula de constructors

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -9,10 +9,17 @@ public class AppClasses {
         cliente1.setSexo('M');
         cliente1.setanoNascimento(1993);
 
-        System.out.println();
         System.out.println("Renda: " + cliente1.getRenda());
         System.out.println("Sexo: " + cliente1.getSexo());
         System.out.println("Nascimento: " + cliente1.getanonascimento());
+        System.out.println("Especial: " + cliente1.isEspecial());
+
+        Cliente cliente2 = new Cliente(20000,'F');
+
+        System.out.println("Renda: " + cliente2.getRenda());
+        System.out.println("Sexo: " + cliente2.getSexo());
+        System.out.println("Nascimento: " + cliente2.getanonascimento());
+        System.out.println("Especial: " + cliente2.isEspecial());
         
     }
 }
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -4,8 +4,49 @@ public class Cliente {
     private double renda; //atributo private, so a classe pode chamar o cliente.renda direto. outros tem que usar o setter;
     private char sexo;
     private int anoNascimento;
+    private boolean especial;
+    
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
 
-    public double getRenda(){ //public permite que esses atributos sejam acessados por codigo fora do mesmo pacote, se sem nada so acessa de dentro do pacote
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    //getEspecial
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+    //construtor sem parametro;
+    public Cliente(){
+        System.out.println("\n" + "criando cliente com construtor sem parametro");
+        double aleatorio = Math.random();
+        if(aleatorio > 0.5){
+            this.especial = true; 
+        };
+    };
+
+    //construtor com parametro; que chama o construtor mais simples e adiciona funcionalidades
+    public Cliente(double renda, char sexo){
+
+        // OBRIGATORIO SER A PRIMEIRA LINHA;
+        //assim se chama construtor dentro de construtor, se diferenciam pelos parametros colocados nos this. sem parametro chama o constr. sem parametro;
+        this(); 
+        System.out.println("\n" + "criando cliente com construtor COM parametro");
+
+        //chamando os sets sem copiar as logicas de validacao aqui novamente!!;
+        setRenda(renda);
+        setSexo(sexo);
+    };
+
+    //public permite que esses atributos sejam acessados por codigo fora do mesmo pacote, se sem nada so acessa de dentro do pacote
+    public double getRenda(){ 
         return renda;
     };
 
