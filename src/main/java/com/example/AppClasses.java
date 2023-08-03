77f5f7f3205208fc4c1c805ed9297d90e87013f8
Guilherme Carlos <guilherme.fpedrinha@gmail.com>
2023-08-03 18:26:29 +0000

    Após aula de classe String

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -5,21 +5,39 @@ public class AppClasses {
     public static void main(String[] args) {
 
         Cliente cliente1 = new Cliente();// instaciana de um novo objetivo do tipo cliente
+        cliente1.setCpf("99999999999");
+        cliente1.setNome("Tarkone Mosin Santos BP");
         cliente1.setRenda(15000);
         cliente1.setSexo('M');
         cliente1.setanoNascimento(1993);
 
+        //caso de uso do split() retornando uma vetor e usando var pra reserver o tipo String[] automaticamente;
+        var vetorNome = cliente1.getNome().split(" ");
+        System.out.println(vetorNome[0] + " " + vetorNome[1]);
+
+        //caso de uso do charAt() para pegar o char em determinada posicao da String
+        var posicaoChar = cliente1.getNome().charAt(0);
+        System.out.println("o Char na posicao 0 é: " + posicaoChar);
+
+        System.out.println("Nome: " + cliente1.getNome());
         System.out.println("Renda: " + cliente1.getRenda());
         System.out.println("Sexo: " + cliente1.getSexo());
         System.out.println("Nascimento: " + cliente1.getanonascimento());
         System.out.println("Especial: " + cliente1.isEspecial());
 
-        Cliente cliente2 = new Cliente(20000,'F');
+        Cliente cliente2 = new Cliente("Ak47",20000,'F');
+        cliente2.setCpf("99999999990");
 
+        System.out.println("Nome: " + cliente2.getNome());
         System.out.println("Renda: " + cliente2.getRenda());
         System.out.println("Sexo: " + cliente2.getSexo());
         System.out.println("Nascimento: " + cliente2.getanonascimento());
         System.out.println("Especial: " + cliente2.isEspecial());
+
+        // nao usar == para comparar strongs. nao funciona. usar .equals server para comparar conteudos, NAO SO STRINGS!
+        if(cliente1.getCpf().equals(cliente2.getCpf())){
+            System.out.println("os cps sao iguais!");
+        }else System.out.println("cpfs distintos");;
         
     }
 }
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -1,11 +1,14 @@
 package com.example;
 
 public class Cliente {
+    private String nome;
     private double renda; //atributo private, so a classe pode chamar o cliente.renda direto. outros tem que usar o setter;
     private char sexo;
     private int anoNascimento;
     private boolean especial;
+    private String cpf; 
     
+
     public int getAnoNascimento() {
         return anoNascimento;
     }
@@ -33,19 +36,40 @@ public class Cliente {
     };
 
     //construtor com parametro; que chama o construtor mais simples e adiciona funcionalidades
-    public Cliente(double renda, char sexo){
+    public Cliente(String nome, double renda, char sexo){
 
         // OBRIGATORIO SER A PRIMEIRA LINHA;
         //assim se chama construtor dentro de construtor, se diferenciam pelos parametros colocados nos this. sem parametro chama o constr. sem parametro;
         this(); 
         System.out.println("\n" + "criando cliente com construtor COM parametro");
 
+        setNome(nome);
         //chamando os sets sem copiar as logicas de validacao aqui novamente!!;
         setRenda(renda);
         setSexo(sexo);
     };
 
     //public permite que esses atributos sejam acessados por codigo fora do mesmo pacote, se sem nada so acessa de dentro do pacote
+    
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    // usando metodos da classes string um depois do outro. trim tira espacos;
+    public void setNome(String nome) {
+        if(nome.isBlank()){
+            System.out.println("O nome é obrigatorio.");
+        }else this.nome = nome.toUpperCase().trim();;
+    }
+    
     public double getRenda(){ 
         return renda;
     };
