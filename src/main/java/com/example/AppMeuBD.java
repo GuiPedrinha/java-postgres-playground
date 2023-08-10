b0cf4873383c5e3f68ef0362f4d450dee5f134e8
Guilherme Carlos <guilherme.fpedrinha@gmail.com>
2023-08-10 18:28:02 +0000

    meuBD refatorado

--- src/main/java/com/example/AppMeuBD.java
+++ src/main/java/com/example/AppMeuBD.java
@@ -1,26 +1,37 @@
 package com.example;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class AppMeuBD {
+    private static final String PASSWORD = "";
+    private static final String USERNAME = "gitpod";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
+
     public static void main(String[] args) {
-        
-        //Carregando driver;
-        try {
-            Class.forName("org.postgresql.Driver");
-            System.out.println("driver carregado com sucesso");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Nao foi possivel carregar o Driver");
+        new AppMeuBD();
+    }
+
+    //constructor;
+    public AppMeuBD(){
+        try(var conn = openConnection()) {
+            loadBdDriver(conn);
+            listarCidades(conn);
+        } catch (SQLException e) {
+            System.err.println("Conexao nao pode ser criada com o banco de dados");
         }
+    }
 
-        //abrir conexao autoclosable com DriverManager;
-        try(var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "")) {
-            System.out.println("Conexao efetuada com sucesso");
 
+
+
+    private void listarCidades(Connection conn) {
+        Statement statement = null;
+        try{
             //criar statement java sql NAO JAVA BEANS!;
-            Statement statement = conn.createStatement();
+            statement = conn.createStatement();
 
             //usar statement para pegar resultSet;
             var result = statement.executeQuery("select * from cidade");
@@ -28,14 +39,36 @@ public class AppMeuBD {
             //imprimir result com whilte e result.next;
             while(result.next()){
                 //com printf;
-                System.out.printf("Id:");
+                System.out.printf("Id: %d Nome: %s Capital: %s \n", result.getInt("id"), result.getString("nome"), result.getString("capital"));
             }
 
         } catch (SQLException e) {
-            System.out.println("nao foi possivel abrir conexao");
-
+            if(statement == null){
+                System.err.println("nao foi possivel abrir conexao");
+            }else
+            System.err.println("Sintaxe sql errada.");
         }
+    }
 
 
+
+
+
+    private Connection openConnection() throws SQLException{
+            System.out.println("Conexao efetuada com sucesso");
+            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+
+
+
+
+
+    private void loadBdDriver(Connection conn) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("driver carregado com sucesso");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Nao foi possivel carregar o Driver");
+        }
     }
 }
