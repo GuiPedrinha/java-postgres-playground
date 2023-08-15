b90eb80774511c6c190b984d82a5ed6b108ef983
Guilherme Carlos <guilherme.fpedrinha@gmail.com>
2023-08-15 19:38:39 +0000

    apos delete do crud

--- src/main/java/com/example/AppBdclean.java
+++ src/main/java/com/example/AppBdclean.java
@@ -39,6 +39,8 @@ public class AppBdclean {
 
             inserirProduto(conn, produto);
 
+            excluirProduto(conn, 202L);
+
             //listando colunas da tabela cliente;
             listaDadosTabela(conn, "produto");
             listaTabelas(conn);
@@ -50,135 +52,156 @@ public class AppBdclean {
         }
     }
 
-  private void inserirProduto(Connection conn, Produto produto) {
+    private void excluirProduto(Connection conn, long id) {
 
-        // em objetos nao se usa o nome com underline tipo marca_id;
-        //cria-se um atributo do tipo Marca marca e ai sim seta o id dentro desse novo obj marca;
-        //diferente do nome que vai no sql que é o nome da coluna msm do banco; sendo marca_id;
-        //marca_id equivale a produto.getMarca().getId();
-        String sql = "INSERT INTO produto (nome, marca_id, valor) VALUES (?, ?, ?)";
+        String sql = "DELETE FROM produto WHERE id = ?";
         try {
+            //preparar statement
             var statement = conn.prepareStatement(sql);
-            //setando o valor das variaveis;
-            statement.setString(1, produto.getNome());
-            statement.setLong(2, produto.getMarca().getId());
-            statement.setDouble(3, produto.getValor());
 
-            //tudo setado agora executar; mas nao com executeQuery e sim com executeUpdate;
-            //executeUpdate serve para comandos DATA MANIPULATION LANG = INSERT UPDATE DELETE;
-            //como usou o conn.prepareStatement(sql) nao vai paramentro dentro do statemente.executeUpdate();
+            //setar parametros ?;
+            statement.setLong(1, id);
+
+            //executando DML;
             statement.executeUpdate();
+
+
         } catch (SQLException e) {
-            System.err.println("Comando SQL incorreto");
+            System.err.println("O comando SQL está incorreto");
+        }
+        
+
         }
 
-    }
+    private void inserirProduto(Connection conn, Produto produto) {
 
-private void listaTabelas(Connection conn) {
-        var sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'";
-        try {
-            var statement = conn.createStatement();
-            var result = statement.executeQuery(sql);
+            // em objetos nao se usa o nome com underline tipo marca_id;
+            //cria-se um atributo do tipo Marca marca e ai sim seta o id dentro desse novo obj marca;
+            //diferente do nome que vai no sql que é o nome da coluna msm do banco; sendo marca_id;
+            //marca_id equivale a produto.getMarca().getId();
+            String sql = "INSERT INTO produto (nome, marca_id, valor) VALUES (?, ?, ?)";
+            try {
+                var statement = conn.prepareStatement(sql);
+                //setando o valor das variaveis;
+                statement.setString(1, produto.getNome());
+                statement.setLong(2, produto.getMarca().getId());
+                statement.setDouble(3, produto.getValor());
 
-            System.out.printf("%s \n", sql);
-            while (result.next()) {
-                System.out.printf("%s \n", result.getString("table_name"));
+                //tudo setado agora executar; mas nao com executeQuery e sim com executeUpdate;
+                //executeUpdate serve para comandos DATA MANIPULATION LANG = INSERT UPDATE DELETE;
+                //como usou o conn.prepareStatement(sql) nao vai paramentro dentro do statemente.executeUpdate();
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Comando SQL incorreto");
             }
-        } catch (SQLException e) {
-            System.err.println("Erro de consulta SQL " + e.getMessage());
+
         }
 
-    }
+    private void listaTabelas(Connection conn) {
+            var sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'";
+            try {
+                var statement = conn.createStatement();
+                var result = statement.executeQuery(sql);
 
-private void listaDadosTabela(Connection conn, String tabela) {
-        // aqui pode concatenar oq nao é dado inserido por usuario;
-        var sql = "select * from " + tabela;
-        System.out.printf("Query: %s \n", sql);
-        System.out.println();
-
-        try {
-            // como nao teve ? no sql ,usar o conn.createStatement normalmente, passa sql como parametro pro execute;
-            var statement = conn.createStatement();
-            var result = statement.executeQuery(sql);
-
-            //criado para simplificar e chamar so uma vez;
-            //result.getMetaData.getColumCount e name;
-            var metadata = result.getMetaData();
-            int colunas = metadata.getColumnCount();
-
-            System.out.println();
-            System.out.printf("A contagem de colunas da tabela %s é: %d", tabela, colunas);
-            System.out.println();
-
-            // *** printando nome das colunas; LEMBRAR i começa em 1 no SQL *** ;
-            for (int i = 1; i <= colunas; i++) {
-                System.out.printf("%-25s ", metadata.getColumnName(i));
+                System.out.printf("%s \n", sql);
+                while (result.next()) {
+                    System.out.printf("%s \n", result.getString("table_name"));
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro de consulta SQL " + e.getMessage());
             }
+
+        }
+
+    private void listaDadosTabela(Connection conn, String tabela) {
+            // aqui pode concatenar oq nao é dado inserido por usuario;
+            var sql = "select * from " + tabela;
+            System.out.printf("Query: %s \n", sql);
             System.out.println();
 
-            // precisa de for pq vamos printar todas as colunas de cada linha;
-            // result.next() vai p/ cada linha e o for pra cada coluna na linha;
-            while (result.next()) {
-                // devolve o numero de colunas de cada linha pra usar dentro do for;
-                // ** no sql i começa com 1 e nao com zero, pq é indice de coluna;
+            try {
+                // como nao teve ? no sql ,usar o conn.createStatement normalmente, passa sql como parametro pro execute;
+                var statement = conn.createStatement();
+                var result = statement.executeQuery(sql);
+
+                //criado para simplificar e chamar so uma vez;
+                //result.getMetaData.getColumCount e name;
+                var metadata = result.getMetaData();
+                int colunas = metadata.getColumnCount();
+
+                System.out.println();
+                System.out.printf("A contagem de colunas da tabela %s é: %d", tabela, colunas);
+                System.out.println();
+
+                // *** printando nome das colunas; LEMBRAR i começa em 1 no SQL *** ;
                 for (int i = 1; i <= colunas; i++) {
-                    //trazer tudo em string so pra visualizar as tabelas, pela getString(index);
-                    System.out.printf("%-25s ", result.getString(i));
+                    System.out.printf("%-25s ", metadata.getColumnName(i));
                 }
                 System.out.println();
-            }
 
-        } catch (Exception e) {
-            System.err.println("Erro de consulta SQL: " + e.getMessage());
-        }
-        System.out.println();
-    }
+                // precisa de for pq vamos printar todas as colunas de cada linha;
+                // result.next() vai p/ cada linha e o for pra cada coluna na linha;
+                while (result.next()) {
+                    // devolve o numero de colunas de cada linha pra usar dentro do for;
+                    // ** no sql i começa com 1 e nao com zero, pq é indice de coluna;
+                    for (int i = 1; i <= colunas; i++) {
+                        //trazer tudo em string so pra visualizar as tabelas, pela getString(index);
+                        System.out.printf("%-25s ", result.getString(i));
+                    }
+                    System.out.println();
+                }
 
-private void localizarEstado(Connection conn, String uf){
-        try {
-            String sql = "select * from estado where uf = ?";
-            // nao usa mais o createStatement, usar o prepareStatement pq recebe um sql como parametro;
-            var statement = conn.prepareStatement(sql);
-            //setString, pq uf é string, para atribuir o valor de ?;
-            // atribuindo a string uf para o parametro ? na posicao 1, que é o primeiro que aparece;
-            statement.setString(1, uf);
-            // usando prepareStatement o executeQuery nao recebe o sql nos parametros;
-            var result = statement.executeQuery();
-            System.out.printf("Query: %s \n", sql);
-            if(result.next()) {
-                System.out.printf("Estado localizado: id: %d nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
-            }
-        } catch (SQLException e) {
-            System.err.println("Comando SQL incorreto");
-        }
-        // pulando linha;
-        System.out.println();
-
-    }
-
-  private void listaEstados(Connection conn) {
-
-        // agora sao trys independentes; refatorar em metodos separados;
-        //p/ saber se erro foi na conexao ou no comando sql declarar variavel statment fora do bloco;
-        // cuidado com imports. é statement sql nao beans;
-        //inicializando statement. sem var agora pq var so intra bloco funciona;
-        //REFATORADO - apos pronto refactor -> extract to method;
-
-        try {
-            System.out.println("Conexao com o banco de dados realizada com sucesso!");
-            
-            var statement = conn.createStatement();
-            var result = statement.executeQuery("select * from estado");
-            while (result.next()) {
-            System.out.printf("Id: %d Nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            } catch (Exception e) {
+                System.err.println("Erro de consulta SQL: " + e.getMessage());
             }
             System.out.println();
-        //tratamento diferenciando erro de conexao com erro de sintaxe da consulta sql;    
-        } catch (SQLException e) {
-             System.err.println(" Erro ao realizar o comando query sql ");
-            //pode usar System.err.println pra mostrar a msg em vermelho de erro;
         }
-    }
+
+    private void localizarEstado(Connection conn, String uf){
+            try {
+                String sql = "select * from estado where uf = ?";
+                // nao usa mais o createStatement, usar o prepareStatement pq recebe um sql como parametro;
+                var statement = conn.prepareStatement(sql);
+                //setString, pq uf é string, para atribuir o valor de ?;
+                // atribuindo a string uf para o parametro ? na posicao 1, que é o primeiro que aparece;
+                statement.setString(1, uf);
+                // usando prepareStatement o executeQuery nao recebe o sql nos parametros;
+                var result = statement.executeQuery();
+                System.out.printf("Query: %s \n", sql);
+                if(result.next()) {
+                    System.out.printf("Estado localizado: id: %d nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+                }
+            } catch (SQLException e) {
+                System.err.println("Comando SQL incorreto");
+            }
+            // pulando linha;
+            System.out.println();
+
+        }
+
+    private void listaEstados(Connection conn) {
+
+            // agora sao trys independentes; refatorar em metodos separados;
+            //p/ saber se erro foi na conexao ou no comando sql declarar variavel statment fora do bloco;
+            // cuidado com imports. é statement sql nao beans;
+            //inicializando statement. sem var agora pq var so intra bloco funciona;
+            //REFATORADO - apos pronto refactor -> extract to method;
+
+            try {
+                System.out.println("Conexao com o banco de dados realizada com sucesso!");
+                
+                var statement = conn.createStatement();
+                var result = statement.executeQuery("select * from estado");
+                while (result.next()) {
+                System.out.printf("Id: %d Nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+                }
+                System.out.println();
+            //tratamento diferenciando erro de conexao com erro de sintaxe da consulta sql;    
+            } catch (SQLException e) {
+                System.err.println(" Erro ao realizar o comando query sql ");
+                //pode usar System.err.println pra mostrar a msg em vermelho de erro;
+            }
+        }
 
     // foi refatorado pq sempre vai ser chamado isso pra criar uma conexao;
     // usou throws para delegar o tratamento dessa conexao ao metodo que chamar este;
